ac22652919db20a2b9a67ed925dbec29638f4d7b
Jovanovic <remyj@outlook.fr>
2022-10-10 22:23:04 +0200

    feat: :art: psot
    post

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const birds = require('./birds')
 
 const app = express()
 
+const faker = require('faker')
+const bodyParser= require('body-parser')
+
+const users = []
+
+app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.json())
+
 app.use('/birds', birds)
 
 app.get('/', function (req, res) {
@@ -22,6 +30,39 @@ app.get(`/remy`, (req,res) =>{
     ]
   })
 })
+
+for(let i=0; i<10; i++){
+  users.push({
+        email: faker.internet.email()
+  })
+}
+
+app.get('/users', (req,res)=>{
+  res.json({
+    data: users
+  })
+});
+
+app.get('/users/:id', (req,res)=>{
+  const id = req.params.id -1
+  
+  res.json({
+    data: users[id] || null
+  })
+
+});
+
+
+app.post('/users', (req,res) => {
+  const data= req.body
+
+  console.log(data)
+
+  res.json({
+    data: undefined
+  })
+})
+
 
 
 app.listen(3000)
