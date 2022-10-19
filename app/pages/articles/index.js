bd218192bb7c8c7805d64e67b032daa055b9914e
aym00n-djrak <remyj@outlook.fr>
2022-10-19 14:51:41 +0200

    fix: link

--- app/pages/articles/index.js
+++ app/pages/articles/index.js
@@ -4,15 +4,45 @@ import db from './data'
 const Articles = () => {
     const article = db.articles.find( article => article.id)    
     
+
   return (
     <div>
     <Link href={`/articles/${article.id}`}>
     <div>
-      <a><h1>Article : {article.id}</h1>
+      <a><h1>Article : 1</h1>
       </a>
       </div>
       </Link>
-      
+
+      <Link href={`/articles/2`}>
+    <div>
+      <a><h1>Article : 2</h1>
+      </a>
+      </div>
+      </Link>
+
+      <Link href={`/articles/3`}>
+    <div>
+      <a><h1>Article : 3</h1>
+      </a>
+      </div>
+      </Link>
+
+      <Link href={`/articles/4`}>
+    <div>
+      <a><h1>Article : 4</h1>
+      </a>
+      </div>
+      </Link>
+
+      <Link href={`/articles/5`}>
+    <div>
+      <a><h1>Article : 5</h1>
+      </a>
+      </div>
+      </Link>
+
+
       </div>  );
 };
 
