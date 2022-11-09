01f80356f70474f947237653d14a6eabb6596f06
aym00n-djrak <remyj@outlook.fr>
2022-11-09 14:44:00 +0100

    feat: link

--- app/layout/Projects.jsx
+++ app/layout/Projects.jsx
@@ -3,6 +3,10 @@ import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import Link from "next/link";
 import Cyber from "../public/cyber.jpg";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { DiCodeigniter } from "react-icons/di";
+
 
 const Projects = () => {
   return (
@@ -12,15 +16,17 @@ const Projects = () => {
       className="flex grid place-items-center h-screen m-4 p-4"
     >
       <h2>My projects :</h2>
+      <div className="grid grid-cols-3 gap-4">
 
-      <div className="grid grid-cols-3 gap-4 hover:grid-cols-6">
-
-          <div className="bg-red-500 ">01</div>
-          <div className="bg-red-500 ">02</div>
-          <div className="bg-red-500 ">03</div>
-          <div className="bg-red-500 ">04</div>
-          <div className="bg-red-500 ">05</div>
-
+          <div className="bg-red-500 ">
+            <a href="https://github.com/aym00n-djrak"><AiFillGithub size={500}/></a>
+          </div>
+          <div className="bg-red-500 ">
+            <a href="https://fr.linkedin.com/in/r%C3%A9my-jovanovic-73766b185"><FaLinkedinIn size={500}/></a>
+          </div>
+          <div className="bg-red-500 ">
+            <DiCodeigniter size={500} />
+          </div>
       </div>
     </div>
   );
