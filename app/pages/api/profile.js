8a1f13a3e63d5df2601053c064fe9c27b2f87957
aym00n-djrak <remyj@outlook.fr>
2022-11-15 23:21:36 +0100

    feat: api

---
+++ app/pages/api/profile.js
@@ -0,0 +1,11 @@
+
+export default function handler(req, res) {
+    if(false){ // Hardcoded for the future
+      return res.status(401).json('Resource access not authorized')
+    }
+    res.status(200).json({
+      username: 'Rémy',
+      email: 'remyj@outlook.fr'
+    })
+
+  }
