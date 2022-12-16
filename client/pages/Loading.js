18ab9c8eb040eff1c682269a8404dd2a9906193e
JulienMontesinos <shihao@outlook.fr>
2022-12-16 18:41:57 +0100

    refactor: manque d'image

--- client/pages/Loading.js
+++ client/pages/Loading.js
@@ -15,7 +15,7 @@ export default function Loading({ loading }) {
         >
             <div className="w-screen h-screen absolute z-50 bg-white">
                 <div className="absolute left-0 right-0 top-0 bottom-0 m-auto text-center h-12 md:h-14">
-                    <img src="/assert/images/logo.png" className="m-auto h-12 md:h-14 transition-transform animate-spin" alt="Loading..." />
+                    <img src="/public/load.jpg" className="m-auto h-12 md:h-14 transition-transform animate-spin" alt="Loading..." />
                     <div className="pt-2 animate-pulse">Loading...</div>
                 </div>
             </div>
